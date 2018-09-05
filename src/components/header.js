8d47481fbe5b26eb83d0cc8c84ffd730779b022a
kooldrmony <derrickreyes.23@gmail.com>
2018-09-05 07:02:14 -0700

    adding code for native project

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>{props.headerText}</Text>
+		</View>
+		);
+};
+
+const styles = {
+
+	viewStyle: {
+
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 25,
+		backgroundColor: 'red',
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2
+		// elevation: 2,
+		// position: 'relative'
+	},
+
+	textStyle: {
+
+		fontSize: 20
+		// backgroundColor: 'blue',
+		// color: 'white'
+	}
+
+};
+
+export default Header;
